a1e4336b609f1dfdd4eb473d3317c14639b42504
lucasabdouni <lucasabdouni@hotmail.com>
2024-04-12 11:57:04 -0300

    feat: created trigger and animation for card border

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { state, style, trigger } from '@angular/animations';
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../models/tarefa';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
-  styleUrls: ['./lista-tarefas.component.css']
+  styleUrls: ['./lista-tarefas.component.css'],
+  animations: [
+    trigger('highlightedState', [
+      state(
+        'default',
+        style({
+          border: '2px solid #B2B6FF',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          border: '4px solid #B2B6FF',
+        })
+      ),
+    ]),
+  ],
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
   formAberto: boolean = false;
   categoria: string = '';
   validado: boolean = false;
+  indexTarefa = -1;
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
